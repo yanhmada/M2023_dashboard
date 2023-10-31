eba5b6c0054823e88a97eba8ad52c2487dcb5da2
Yanet HM <98841526+yanhmada@users.noreply.github.com>
2023-10-30 19:28:33 -0700

    cambios menores de formato

--- pages/4_Análisis_Exploratorio_de_datos.py
+++ pages/4_Análisis_Exploratorio_de_datos.py
@@ -18,17 +18,8 @@ st.caption('''
 
 st.caption('''
     Del conjunto de datos tomado de Cenace~ Conagua, archivo fuente en formato yyy-mm-dd hh:00:00, desde\
-    2007-01-01 a 2023-09-05.  Contiene 24 variables de demanda de energía, datos reales-medidos  de la \
-    demanda de carga de energía eléctrica en la Zona de Gerencia del  Noroeste por cada hora del día \
-    compuesta por zonas de carga, a saber, Caborca, Nogales, Hermosillo, Guaymas, Cd. Obregón, Navojoa, \
-    Los Mochis, Guasave, Culiacán y Mazatlán, cargas fuera del consumo de las zonas de carga descritas \
-    anteriormente,  el consumo de las centrales y las pérdidas de energía que existen en la red.
-    10 variables de calendario: lunes festivo, lunes después de festivo, Semana Santa, 1 de mayo, 10 de mayo, \
-    16 de septiembre, 2 de noviembre, día antes de Navidad o Año Nuevo, Navidad o Año Nuevo, \
-    día después de Navidad o Año Nuevo.
-    Fuente Comisión Nacional del Agua (CONAGUA):
-    Fecha en formato yyyy-mm-dd, desde 2007-01-01 a 2023-01-18. 14  variables meteorológicas\
-           C and mm/h), datos reales-medidos de la  temperatura máxima y mínima diaria en Caborca, \
+    2007-01-01 a 2023-09-05.  Contiene variables de energía y de calendario, así como 14  variables meteorológicas\
+        de  temperatura máxima y mínima diaria de las ciudades de la zona de carga, que incluye  Caborca, \
            Hermosillo, Ciudad Obregón, Los Mochis y Culiacán. Precipitación diaria en Hermosillo, \
            Ciudad Obregón, Los Mochis y Culiacán.           
            ''')
@@ -47,7 +38,7 @@ y = df_GCRNO_complete['Energy_Demand']
 
 fig = px.box(df_GCRNO_complete, x, y, color=x)
 
-fig.update_layout(title_text="Demanda de Energia por Hora", height=1000)
+fig.update_layout(title_text="Demanda de energia por hora", height=1000)
 fig.update(layout_showlegend=False)
 
 # Display the figure in Streamlit
@@ -66,7 +57,7 @@ fig = px.box(filtered_df, x='Month', y='Energy_Demand',  points='outliers',
                 color='Month',
                 category_orders={'Month': [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11,12]},
                 labels={'Month': 'Mes'},
-                title='Demanda de energía por Mes')
+                title='Demanda de energía por mes')
 fig.update_xaxes(type='category', tickmode='array', tickvals=[1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11,12],
                  ticktext=[month_labels[Month] for Month in range(1,13  )])
 st.plotly_chart(fig)
@@ -79,7 +70,7 @@ fig = px.box(filtered_df, x='Day', y='Energy_Demand',  points='outliers',
                 color='Day',
                 category_orders={'DayOfWeek': [0, 1, 2, 3, 4, 5, 6]},
                 labels={'Day': 'Día de la semana'},
-                title='Demanda de Energía por día de la semana')
+                title='Demanda de energía por día de la semana')
 fig.update_xaxes(type='category', tickmode='array', tickvals=[0, 1, 2, 3, 4, 5, 6],
                  ticktext=[day_labels[day] for day in range(7)])
 st.plotly_chart(fig)
